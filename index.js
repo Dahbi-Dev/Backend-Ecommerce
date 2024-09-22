4fb84547d9716fad6b8c6d6e4c740e9b4fbec6b8
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-09-23 00:21:44 +0100

    updatte

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `https://backend-ecommerce-gibj.onrender.com:${port}/images/${req.file.filename}`,
+    image_url: `https://backend-ecommerce-gibj.onrender.com/images/${req.file.filename}`,
   });
 });
 
